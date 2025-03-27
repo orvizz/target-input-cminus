6fbf7a6603243c678a959f0011a5861b96c3bd49
orvizz <marioorvvie@gmail.com>
2025-03-27 16:04:31 +0100

    Added array and structs for cast operation

--- cast.py
+++ cast.py
@@ -1,17 +1,19 @@
 import random
-allExpr = ['a', 'b', 'c', "'a'", 1, 1.5, 'd']
+allExpr = ['a', 'b', 'c', "'a'", 1, 1.5, 'd', 'iArr', 'pair']
 types = ["int", "char", "double"]
 
 with open('target/l9-cast.cminus', 'w') as f:
     f.write("void main() {\n")
-    f.write("\tint a;\n\tdouble b;\n\tchar c;\n")
+    f.write("\tint a;\n\tdouble b;\n\tchar c;\n\tint[10] iArr;\n\tstruct {\n\t\tint a;\n\t\tint b;\n\t} pair;\n")
     f.write("\n")
-    line = 5
+    line = 10
     for expr in allExpr:
         assign = f'a = ({types[random.randint(0,2)]}) ' + str(expr) + ';'
         line+=1
-        if expr == 'd':
+        if expr == 'd' or expr == 'iArr' or expr == 'pair':
             assign += f" // Error in line {line}"
         assign+= "\n"
         f.write(f"\t{assign}")
+    f.write(f"\ta = (int[10]) iArr; // Error in line {line+1}\n")
+    f.write(f"\tiArr = (int[10]) 1; // Error in line {line+1}\n")
     f.write("}\n")
